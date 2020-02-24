b60821291e29f58ccea3788af40f501ab93dfd5d
조연관 <@LAPTOP-66VTEHKF>
2020-02-24 15:09:26 +0900

    1

---
+++ spring-boot-test/src/main/java/com/example/demo/SampleController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+ 
+    @RequestMapping(value="/")
+    @ResponseBody
+    public String sampleHome() {
+        
+        return "Hello Spring!";
+        
+    }
+}
